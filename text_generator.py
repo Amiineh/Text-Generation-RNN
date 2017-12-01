21d14fa52d61055c686ea2e14fe2e1525b11c6b8
amineh.ahm <amineh.ahm@gmail.com>
2017-12-01 20:29:18 +0330

    debugging

--- text_generator.py
+++ text_generator.py
@@ -121,14 +121,19 @@ for epoch in range(epoch_size):
     # Predict next 400 characters:
     res_chars = ''
     for i in range(400):
-        if (i != 0):
-            x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(predicted, [1, 1, dict_size]))
+        # Predict the next character:
         x_test = np.reshape(x_test, [1, seq_len, dict_size])
         predicted = sess.run(prediction, feed_dict={x: x_test})
         predicted_char = idx_to_char[np.argmax(predicted, 0)]
         res_chars += predicted_char
+        # Add predicted char to x_test sequence:
+        # print (predicted)
+        # print (predicted_char)
+        x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(predicted, [1, 1, dict_size]), axis=1)
 
+        # print (x_test.shape)
         # x_test_idx = []
+        # # x_test_idx = [np.argmax(w) for w in x_test]
         # for w in x_test[0]:
         #     x_test_idx.append(np.argmax(w))
         # print (x_test_idx)
