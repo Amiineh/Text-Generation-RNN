49a8208111e72ff6440d2c03f0f37c44cfdd11e5
amineh.ahm <amineh.ahm@gmail.com>
2017-12-01 11:50:46 +0330

    added test

--- text_generator.py
+++ text_generator.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
-from tensorflow.contrib import layers
+from random import *
 import numpy as np
 
 # Read data:
@@ -18,19 +18,20 @@ stride = 3
 hidden_size = 128
 epoch_size = 20
 batch_size = 100
+test_size = 5
 
 # Prepare training data:
-train = []
-real = []
+train_data = []
+target_data = []
 for i in range(0, int((text_len - seq_len)/stride) + 1, seq_len - stride):
-    train.append([char_to_idx[ch] for ch in text[i:i+seq_len]])
-    real.append(char_to_idx[text[i+seq_len]])
+    train_data.append([char_to_idx[ch] for ch in text[i:i+seq_len]])
+    target_data.append(char_to_idx[text[i+seq_len]])
 
-x_train = np.zeros((len(train), seq_len, dict_size))
-y_train = np.zeros((len(train), dict_size))
+x_train = np.zeros((len(train_data), seq_len, dict_size))
+y_train = np.zeros((len(train_data), dict_size))
 
-for seq_index, seq in enumerate(train):
-    y_train[seq_index, real[seq_index]] = 1
+for seq_index, seq in enumerate(train_data):
+    y_train[seq_index, target_data[seq_index]] = 1
     for i in range(seq_len):
         x_train[seq_index, i, seq[i]] = 1
 
@@ -54,7 +55,7 @@ predictions = tf.matmul(outputs[-1], softmax_w) + softmax_b
 # w = tf.ones((batch_size, seq_len))
 # loss = tf.reduce_mean(tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=y, weights= w))
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predictions))
-train = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+train_op = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
 
 # Start training:
 config = tf.ConfigProto()
@@ -68,9 +69,37 @@ for epoch in range(epoch_size):
         batch_x = x_train[i:i+batch_size]
         batch_y = y_train[i:i+batch_size]
 
-        sess.run(train, feed_dict={x:batch_x, y: batch_y})
+        sess.run(train_op, feed_dict={x:batch_x, y: batch_y})
 
-    print ("Epoch #%d\t Loss: %f" % (epoch, sess.run(loss, feed_dict={x:x_train, y:y_train})))
+    print ("Epoch #%d\t Loss: %f" % (epoch+1, sess.run(loss, feed_dict={x:x_train, y:y_train})))
 
+# Test the model:
+for i in range(test_size):
+    print ("\n\n-------------------------------------------------------- Test #%d --------------------------------------------------------"%(i+1))
+    print ("Input: ")
+    test_input_chars = ''
+    rand_idx = randint(0, len(x_train))
+    x_test = x_train[rand_idx]
+    x_test_idx = [np.where(w==1)[0][0] for w in x_test]
+    for w in train_data[rand_idx]:
+        test_input_chars += idx_to_char[w]
 
+    print ("\"%s\"" %(test_input_chars))
+
+    # Predict next 400 characters:
+    res_chars = ''
+    for i in range(400):
+        if (i != 0):
+            x_test[0, 0:-2] = x_test[0, 1:-1]
+            x_test[0, -1] = predicted
+        x_test = np.reshape(x_test, [1, seq_len, dict_size])
+        predicted = sess.run(predictions, feed_dict={x: x_test})
+        predicted_char = idx_to_char[np.argmax(predicted, 1)[0]]
+
+        # x_test_idx = []
+        # for w in x_test[0]:
+        #     x_test_idx.append(np.argmax(w))
+
+    print ("\nGenerated text: ")
+    print ("\"%s\"" %(res_chars))
 
