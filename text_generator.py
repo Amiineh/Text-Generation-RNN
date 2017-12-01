e6e7ec6d51f4e7550268ff5eb42e69bc365f9247
amineh.ahm <amineh.ahm@gmail.com>
2017-12-01 15:12:01 +0330

    chenged y

--- text_generator.py
+++ text_generator.py
@@ -28,33 +28,49 @@ for i in range(0, int((text_len - seq_len)/stride) + 1, seq_len - stride):
     target_data.append(char_to_idx[text[i+seq_len]])
 
 x_train = np.zeros((len(train_data), seq_len, dict_size))
-y_train = np.zeros((len(train_data), dict_size))
+y_train = np.zeros((len(train_data), seq_len, dict_size))
 
 for seq_index, seq in enumerate(train_data):
-    y_train[seq_index, target_data[seq_index]] = 1
     for i in range(seq_len):
         x_train[seq_index, i, seq[i]] = 1
+        if i <seq_len-1:
+            y_train[seq_index, i, seq[i+1]] = 1
+
+# x_test_idx = []
+# for w in y_train[0]:
+#     x_test_idx.append(np.argmax(w))
+# print (x_test_idx)
+#
+# x_test_idx = []
+# for w in x_train[0]:
+#     x_test_idx.append(np.argmax(w))
+# print (x_test_idx)
 
 # Model the network:
 x = tf.placeholder(tf.float32, (None, seq_len, dict_size))
 x_flat = tf.reshape(tf.transpose(x, [1, 0, 2]), [-1, dict_size])
 # x_flat = tf.split(x, seq_len, 0)
-y = tf.placeholder(tf.float32, (None, dict_size))
+y = tf.placeholder(tf.float32, (None, seq_len, dict_size))
 
 cell = rnn.GRUCell(hidden_size)
-initial_state = cell.zero_state(batch_size, dtype=tf.float32)
-# outputs, states = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state,dtype=tf.float32)
-outputs, states = rnn.static_rnn(cell, tf.unstack(tf.transpose(x, [1, 0, 2])), dtype=tf.float32)
+# initial_state = cell.zero_state(batch_size, dtype=tf.float32)
+outputs, states = tf.nn.dynamic_rnn(cell, x,dtype=tf.float32)
+# outputs, states = rnn.static_rnn(cell, tf.unstack(tf.transpose(x, [1, 0, 2])), dtype=tf.float32)
+
+outputs_flat = tf.reshape(outputs, [-1, hidden_size])
 
 softmax_w = tf.Variable(tf.truncated_normal([hidden_size, dict_size]))
 softmax_b = tf.Variable(tf.zeros(dict_size))
 
 # predictions = tf.matmul(outputs[:,-1,:], softmax_w) + softmax_b
-predictions = tf.matmul(outputs[-1], softmax_w) + softmax_b
+predictions = tf.matmul(outputs_flat, softmax_w) + softmax_b
 
+prediction = tf.nn.softmax(predictions[-1])
+
+y_flat = tf.reshape(y, [-1, dict_size])
 # w = tf.ones((batch_size, seq_len))
 # loss = tf.reduce_mean(tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=y, weights= w))
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predictions))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_flat, logits=predictions))
 train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
 # Start training:
@@ -90,16 +106,16 @@ for epoch in range(epoch_size):
     res_chars = ''
     for i in range(400):
         if (i != 0):
-            x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(predicted, [1, 1, dict_size]), axis=1)
+            x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(predicted, [1, 1, dict_size]))
         x_test = np.reshape(x_test, [1, seq_len, dict_size])
-        predicted = sess.run(predictions, feed_dict={x: x_test})
-        predicted_char = idx_to_char[np.argmax(predicted, 1)[0]]
+        predicted = sess.run(prediction, feed_dict={x: x_test})
+        predicted_char = idx_to_char[np.argmax(predicted, 0)]
 
         res_chars += predicted_char
-        x_test_idx = []
-        for w in x_test[0]:
-            x_test_idx.append(np.argmax(w))
-        print (x_test_idx)
+        # x_test_idx = []
+        # for w in x_test[0]:
+        #     x_test_idx.append(np.argmax(w))
+        # print (x_test_idx)
 
     print ("\nGenerated text: ")
     print ("\"%s\"\n\n" %(res_chars))
