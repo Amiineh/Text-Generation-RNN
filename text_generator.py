1b0b9bf038ea2a58184f973061c3da62a1b48271
amineh.ahm <amineh.ahm@gmail.com>
2017-12-02 22:10:52 +0330

    .py on master

--- text_generator.py
+++ text_generator.py
@@ -3,6 +3,14 @@ from tensorflow.contrib import rnn
 from random import *
 import numpy as np
 
+def sample(a):
+    a = np.log(a) / 0.5
+    dist = np.exp(a)/np.sum(np.exp(a))
+    choices = range(len(a))
+    res = np.zeros_like(a, np.float32)
+    res[np.random.choice(choices, p=dist)] = 1
+    return res
+
 # Read data:
 text = open("nietzsche.txt", 'r').read().lower()
 chars = sorted(list(set(text)))
@@ -16,7 +24,7 @@ learning_rate = 0.01
 seq_len = 40
 stride = 3
 hidden_size = 128
-epoch_size = 20
+epoch_size = 50
 batch_size = 100
 test_size = 5
 
@@ -28,34 +36,42 @@ for i in range(0, int((text_len - seq_len)/stride) + 1, seq_len - stride):
     target_data.append(char_to_idx[text[i+seq_len]])
 
 x_train = np.zeros((len(train_data), seq_len, dict_size))
-y_train = np.zeros((len(train_data), dict_size))
+y_train = np.zeros((len(train_data), seq_len, dict_size))
 
 for seq_index, seq in enumerate(train_data):
-    y_train[seq_index, target_data[seq_index]] = 1
     for i in range(seq_len):
         x_train[seq_index, i, seq[i]] = 1
+        if i <seq_len-1:
+            y_train[seq_index, i, seq[i+1]] = 1
+        else:
+            y_train[seq_index, i,target_data[seq_index]] = 1
 
 # Model the network:
 x = tf.placeholder(tf.float32, (None, seq_len, dict_size))
-x_flat = tf.reshape(tf.transpose(x, [1, 0, 2]), [-1, dict_size])
-# x_flat = tf.split(x, seq_len, 0)
-y = tf.placeholder(tf.float32, (None, dict_size))
+y = tf.placeholder(tf.float32, (None, seq_len, dict_size))
 
 cell = rnn.GRUCell(hidden_size)
-initial_state = cell.zero_state(batch_size, dtype=tf.float32)
-# outputs, states = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state,dtype=tf.float32)
-outputs, states = rnn.static_rnn(cell, tf.unstack(tf.transpose(x, [1, 0, 2])), dtype=tf.float32)
+outputs, states = tf.nn.dynamic_rnn(cell, x,dtype=tf.float32)
+tf.summary.histogram("outputs", outputs)
 
-softmax_w = tf.Variable(tf.truncated_normal([hidden_size, dict_size]))
+outputs_flat = tf.reshape(outputs, [-1, hidden_size])
+
+softmax_w = tf.Variable(tf.truncated_normal([hidden_size, dict_size], mean=0, stddev=0.1))
 softmax_b = tf.Variable(tf.zeros(dict_size))
+tf.summary.histogram("w", softmax_w)
+tf.summary.histogram("b", softmax_b)
 
-# predictions = tf.matmul(outputs[:,-1,:], softmax_w) + softmax_b
-predictions = tf.matmul(outputs[-1], softmax_w) + softmax_b
+predictions = tf.matmul(outputs_flat, softmax_w) + softmax_b
+tf.summary.histogram("predictions", predictions)
 
-# w = tf.ones((batch_size, seq_len))
-# loss = tf.reduce_mean(tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=y, weights= w))
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predictions))
-train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+predictions_flat = tf.reshape(predictions, [-1, dict_size])
+y_flat = tf.reshape(y, [-1, dict_size])
+tf.summary.histogram("targets", y)
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_flat, logits=predictions_flat))
+train_op = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+
+prediction = tf.nn.softmax(predictions[-1])
 
 # Start training:
 config = tf.ConfigProto()
@@ -64,19 +80,35 @@ sess = tf.Session(config=config)
 
 sess.run(tf.global_variables_initializer())
 
+# Show summaries:
+writer = tf.summary.FileWriter('./log/', sess.graph)
+merge = tf.summary.merge_all()
+
+# Save the model:
+saver = tf.train.Saver()
+saver.restore(sess=sess, save_path="./save/model")
+
 for epoch in range(epoch_size):
+    saver.save(sess, "./save/model")
+
     for i in range(0, len(x_train), batch_size):
         batch_x = x_train[i:i+batch_size]
         batch_y = y_train[i:i+batch_size]
 
         sess.run(train_op, feed_dict={x:batch_x, y: batch_y})
 
-    print ("Epoch #%d\t Loss: %f" % (epoch+1, sess.run(loss, feed_dict={x:x_train, y:y_train})))
 
-# Test the model:
-# for i in range(test_size):
-    print ("-------------------------------------------------------- Test #%d --------------------------------------------------------"%(epoch+1))
-    print ("Input: ")
+    # Test the model:
+    print ("-------------------------------------------------------- Epoch %d --------------------------------------------------------"%(epoch+1))
+
+    report_loss, merge_smry = sess.run([loss, merge], feed_dict={x: x_train, y: y_train})
+    print ("Loss: %f" % (report_loss))
+    tf.summary.scalar("loss", report_loss)
+    smry = tf.Summary(value=[tf.Summary.Value(tag="loss", simple_value=report_loss)])
+    writer.add_summary(smry, epoch)
+    writer.add_summary(merge_smry, epoch)
+
+    print ("\nInput: ")
     test_input_chars = ''
     rand_idx = randint(0, len(x_train))
     x_test = x_train[rand_idx]
@@ -89,17 +121,14 @@ for epoch in range(epoch_size):
     # Predict next 400 characters:
     res_chars = ''
     for i in range(400):
-        if (i != 0):
-            x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(predicted, [1, 1, dict_size]), axis=1)
+        # Predict the next character:
         x_test = np.reshape(x_test, [1, seq_len, dict_size])
-        predicted = sess.run(predictions, feed_dict={x: x_test})
-        predicted_char = idx_to_char[np.argmax(predicted, 1)[0]]
-
-        res_chars += predicted_char
-        x_test_idx = []
-        for w in x_test[0]:
-            x_test_idx.append(np.argmax(w))
-        print (x_test_idx)
+        predicted = sess.run(prediction, feed_dict={x: x_test})
+        probs = sample(predicted)
+        probs_char = idx_to_char[np.argmax(probs)]
+        res_chars += probs_char
+        # Add predicted char to x_test sequence:
+        x_test = np.append(np.delete(x_test, 0, axis=1), np.reshape(probs, [1, 1, dict_size]), axis=1)
 
     print ("\nGenerated text: ")
     print ("\"%s\"\n\n" %(res_chars))
